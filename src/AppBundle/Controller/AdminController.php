4cbc7dac4be0684078bdf189749b4c39c6649fa0
lukinias <lemartin79@yahoo.com.ar>
2020-03-01 21:02:20 -0300

    add security controller, login and logout actions

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -10,12 +10,26 @@ use AppBundle\Entity\Usuario;
 class AdminController extends Controller
 {
     /**
+     * @Route("/admin", name="admin")
+     */
+    public function adminAction(Request $request)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        return $this->render('admin/admin.html.twig', array(
+            'active_menu' => '1'
+        ));
+    }
+
+    /**
      * @Route("/listado-cursos", name="listado-cursos")
      */
     public function listadoCursosAction(Request $request)
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
         return $this->render('admin/listado-cursos.html.twig', array(
-            //'active_menu' => '7'
+            'active_menu' => '1'
         ));
     }
 
@@ -24,8 +38,34 @@ class AdminController extends Controller
      */
     public function editarCursoAction(Request $request)
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
         return $this->render('admin/editar-curso.html.twig', array(
-            //'active_menu' => '7'
+            'active_menu' => '1'
+        ));
+    }
+
+    /**
+     * @Route("/listado-publicaciones", name="listado-publicaciones")
+     */
+    public function listadoPublicacionesAction(Request $request)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        return $this->render('admin/listado-publicaciones.html.twig', array(
+            'active_menu' => '1'
+        ));
+    }
+
+    /**
+     * @Route("/editar-publicacion", name="editar-publicacion")
+     */
+    public function editarPublicacionAction(Request $request)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        return $this->render('admin/editar-publicacion.html.twig', array(
+            'active_menu' => '1'
         ));
     }
 }
