255f0467f2acb890856221ebe1a4e14b9e0440a0
lukinias <lemartin79@yahoo.com.ar>
2020-03-07 14:51:11 -0300

    add ckeditor to curso description and render it

--- src/AppBundle/Form/CursoType.php
+++ src/AppBundle/Form/CursoType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CursoType extends AbstractType
@@ -16,10 +18,12 @@ class CursoType extends AbstractType
         $builder
            ->add('nombre', TextType::class)
            ->add('horas', IntegerType::class)
-           ->add('descripcion', TextareaType::class)
+           ->add('descripcion', CKEditorType::class, array('config' => array('toolbar' => 'standard')))
            ->add('mjn', IntegerType::class)
            ->add('mecaba', IntegerType::class)
+           ->add('activo', CheckboxType::class, array('required' => false))
            ->add('cuidi', IntegerType::class)
+
            ->add('guardar', SubmitType::class, ['label' => 'Guardar'])
        ;
     }
